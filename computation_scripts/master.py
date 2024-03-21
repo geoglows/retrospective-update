e614da0fd4e3d4f63627bc22038f48ef8e3c7734
rileyhales <rileyhales1@gmail.com>
2024-03-21 13:00:49 -0600

    use os system

--- computation_scripts/master.py
+++ computation_scripts/master.py
@@ -390,7 +390,7 @@ def fetch_staged_era5():
     # fetch the files
     logging.info(f'Fetching ERA5 runoff files from {s3_era_bucket}')
     logging.info(f'aws s3 cp {s3_era_bucket} {runoff_dir} --recursive --include "*"')
-    subprocess.run(['aws', 's3', 'cp', s3_era_bucket, runoff_dir, '--recursive', '--include', '"*"'], shell=True)
+    os.system(f'aws s3 cp {s3_era_bucket} {runoff_dir} --recursive --include "*"')
     return
 
 
