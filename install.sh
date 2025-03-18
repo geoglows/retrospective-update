71e0c55d50c199bba84937ad5ed0aaa11fb15d01
Ricky Rosas <lrr43@byu.edu>
2025-03-18 17:03:25 -0600

    rfs-v2.1 better installation methods

--- install.sh
+++ install.sh
@@ -8,9 +8,12 @@ sudo apt-get update
 # Download and install Miniconda
 echo "Downloading and installing Miniconda..."
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh
+bash Miniforge3-$(uname)-$(uname -m).sh -b
 ~/miniforge3/bin/conda init --all
 source .bashrc
+
+conda config --add channels conda-forge
+conda config --set channel_priority strict
 
 # Create conda environment from environment file
 echo "Creating conda environment from $ENVIRONMENT_FILE..."
